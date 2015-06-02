5671c74ded7dc355555d49a266238582202bd97c
ananyamart <trulyemphatic@gmail.com>
2015-06-02 13:41:10 +0530

    project 1

--- plot4.R
+++ plot4.R
@@ -1,5 +1,4 @@
-setwd("C:\\Users\\ANANYA\\Downloads\\coursera\\exploratory data analysis\\project")
-data <- "./1/household_power_consumption.txt"
+data <- "household_power_consumption.txt"
 r<- read.table(data, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 read <- subset(r,grepl("^[1|2].2.2007", r$Date)== TRUE)
 datetime <- strptime(paste(read$Date, read$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
